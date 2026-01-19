72285c1192fa7fe79ff9d378d86ba992507852f9
Asadbek To'rayev <asadback25@gmail.com>
2026-01-19 11:32:56 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -90,13 +90,7 @@ REST_FRAMEWORK = {
 }
 
 
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
-    "AUTH_HEADER_TYPES": ("Bearer",),
-}
+
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
